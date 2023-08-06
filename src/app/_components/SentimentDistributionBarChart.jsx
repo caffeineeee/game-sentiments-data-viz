55cde45b83c88724a7f3873019d6d9eed449c880
itscevinsam <cevinsam11@gmail.com>
2023-08-06 23:09:06 +0700

    Add SentimentDistributionBarChart component

---
+++ src/app/_components/SentimentDistributionBarChart.jsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { BarElement, CategoryScale, Chart, LinearScale } from 'chart.js';
+
+Chart.register(LinearScale, CategoryScale, BarElement);
+
+const SentimentDistributionBarChart = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://caxxels.github.io/test-json/test.json');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const getSentimentCounts = () => {
+    const sentimentCounts = {
+      Positive: 0,
+      Neutral: 0,
+      Negative: 0,
+    };
+
+    data.forEach(item => {
+      const sentiment = item.sentiment;
+      sentimentCounts[sentiment]++;
+    });
+
+    return Object.values(sentimentCounts);
+  };
+
+  const labels = ['Positive', 'Neutral', 'Negative'];
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Game Sentiment Analysis',
+        data: getSentimentCounts(),
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+        ],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Count of sentiment',
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: 'sentiment',
+        }
+      },
+    },
+  };
+
+  return (
+    <>
+      <p className='text-sm text-center'>Sentiment Distribution Bar Chart</p>
+      <Bar className='h-64' data={ chartData } options={ options } />
+    </>
+  );
+};
+
+export default SentimentDistributionBarChart;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
+import SentimentDistributionBarChart from "./_components/SentimentDistributionBarChart";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center text-xl">
-        <div className="pt-4">
+        <div>
           <h1>Game Sentiments Data Viz</h1>
         </div>
         <p className="pb-4 text-sm text-gray-300">This project acts as part of Recruitment process for the Front-End Engineer position at CDP</p>
         <div>
+          <div>
+            <SentimentDistributionBarChart />
+          </div>
         </div>
       </div>
     </main>
