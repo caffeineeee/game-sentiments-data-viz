eb932186eac0bd638e8c940c4b284d5f98b88527
itscevinsam <cevinsam11@gmail.com>
2023-08-06 23:27:13 +0700

    Add more interactivity to SentimentDistributionBarChart component

--- src/app/_components/SentimentDistributionBarChart.jsx
+++ src/app/_components/SentimentDistributionBarChart.jsx
@@ -2,9 +2,9 @@
 
 import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
-import { BarElement, CategoryScale, Chart, LinearScale } from 'chart.js';
+import { Chart, LinearScale, CategoryScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-Chart.register(LinearScale, CategoryScale, BarElement);
+Chart.register(LinearScale, CategoryScale, BarElement, Title, Tooltip, Legend);
 
 const SentimentDistributionBarChart = () => {
   const [data, setData] = useState([]);
